e8d124a6dbbd646a4007dc288fd71da17759c8b5
Rane <rane@blasph.me>
2019-06-14 18:52:48 -0700

    stubbing components, installing react-router-dom

---
+++ client/src/components/Joke.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Joke = props => {
+    return <div>Joke</div>;
+}
+
+export default Joke;
---
+++ client/src/components/Jokes.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Jokes = props => {
+    return <div>Jokes</div>;
+}
+
+export default Jokes;
